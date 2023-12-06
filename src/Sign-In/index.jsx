0485df3ca408c1585f2a049641d30021829ded09
rpruazol <rpruazol@gmail.com>
2023-12-05 23:44:21 -0500

    login button basic css

--- src/Sign-In/index.jsx
+++ src/Sign-In/index.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { View, Text, TextInput, } from 'react-native';
+import { View, Text, TextInput, StyleSheet } from 'react-native';
 import { Button } from 'react-native-paper';
 
 const SignInScreen = ({ navigation }) => {
@@ -12,8 +12,9 @@ const SignInScreen = ({ navigation }) => {
   };
 
   return (
-    <View>
+    <View style={styles.container}>
       <Text>Sign In</Text>
+      <View style={styles.inputView}>
       <TextInput
         placeholder="Email"
         value={email}
@@ -25,10 +26,46 @@ const SignInScreen = ({ navigation }) => {
         value={password}
         onChangeText={(text) => setPassword(text)}
       />
-      <Button title="Sign In" onPress={handleSignIn} />
-      <Button title="Go to Sign Up" onPress={() => navigation.navigate('SignUp')} />
+      </View>
+      <View style={styles.button}>
+        <Button mode="outlined" onPress={handleSignIn}>
+          Sign In
+        </Button>
+        <Button mode="outlined" onPress={() => navigation.navigate('SignUp')}>
+          Not a member?  Register
+        </Button>
+      </View>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+
+  container: {
+    flex: 1,
+    backgroundColor: '#5271ff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  inputView: {
+    backgroundColor: "#5271ff",
+    borderRadius: 30,
+    width: "70%",
+    height: 45,
+    marginBottom: 20,
+    alignItems: "center",
+  },
+  TextInput: {
+    height: 50,
+    flex: 1,
+    padding: 10,
+    marginLeft: 20,
+  },
+  button: {
+    color: 'black'
+  }
+
+});
+
 
 export default SignInScreen;
